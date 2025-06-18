ae2d495dc32ffbf902c677a7599fa3533765b6d3
nicolas-magalhaes-clear <nicolas.magalhaes.clear@gmail.com>
2025-06-18 16:37:38 -0300

    fix: using simple network monitor pod values

--- ios/RNNetworkMonitor.swift
+++ ios/RNNetworkMonitor.swift
@@ -1,48 +1,34 @@
 import Foundation
-import SimpleNetworkMonitor
 import React
+import SimpleNetworkMonitor
 
 @objc(RNNetworkMonitor)
 class RNNetworkMonitor: RCTEventEmitter, NetworkMonitorDelegate {
 
-  override init() {
-    super.init()
-    NetworkMonitor.shared.delegate = self
-  }
-
-  @objc
-  func startMonitoring() {
-    NetworkMonitor.shared.startMonitoring()
-  }
-
-  @objc
-  func stopMonitoring() {
-    NetworkMonitor.shared.stopMonitoring()
-  }
-
-  override class func requiresMainQueueSetup() -> Bool {
-    return true
-  }
-
-  override func supportedEvents() -> [String]! {
-    return ["NetworkStatusChanged"]
-  }
-
-  func networkStatusDidChange(_ status: NetworkStatus) {
-    let statusString: String
-    switch status {
-    case .connectedViaWiFi:
-      statusString = "wifi"
-    case .connectedViaCellular:
-      statusString = "cellular"
-    case .disconnected:
-      statusString = "disconnected"
-    case .unknown:
-      fallthrough
-    default:
-      statusString = "unknown"
+    override init() {
+        super.init()
+        NetworkMonitor.shared.delegate = self
     }
 
-    sendEvent(withName: "NetworkStatusChanged", body: ["status": statusString])
-  }
+    @objc
+    func startMonitoring() {
+        NetworkMonitor.shared.startMonitoring()
+    }
+
+    @objc
+    func stopMonitoring() {
+        NetworkMonitor.shared.stopMonitoring()
+    }
+
+    override class func requiresMainQueueSetup() -> Bool {
+        return true
+    }
+
+    override func supportedEvents() -> [String]! {
+        return ["NetworkStatusChanged"]
+    }
+
+    func networkStatusDidChange(_ data: NetworkStatusData) {
+        sendEvent(withName: "NetworkStatusChanged", body: data.toDict())
+    }
 }
